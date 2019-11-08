ec6893ac307a5b30d74aec594e71e8955eec857c
Jouri <Jouri.w.z@gmail.com>
2019-11-08 14:54:23 +0100

    added admin route

---
+++ routes/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+// Profile Routes
+Route::get('/', 'ProfileController@index')->name('admin');
+// Route::resource('restaurants', 'RestaurantController');
+// Route::resource('consumables', 'ConsumableController');
+// Route::resource('profiles', 'ProfileController');
+// Route::get('/{id}/orders', 'ProfileController@orders')->name('orders');
